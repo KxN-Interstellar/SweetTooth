81d23f906d00310d96ecb87e39de04e47e788935
nico-j3 <nicolejuarezflores@gmail.com>
2024-01-23 14:33:28 -0500

    nicole's section

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,8 @@
 // fetchData.js
 
+
+
+
 const allDesserts = fetchData('')
 allDesserts.forEach((dessert) => {
     
@@ -8,9 +11,11 @@ allDesserts.forEach((dessert) => {
 
 
 
-const specificDesert = fetchData('')
-
 
 /*
 Nicole
 */
+
+const specificDesert = fetchData('')
+
+
