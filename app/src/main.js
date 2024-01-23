c5a11621e64e677c45a3b326ab0feceecc0ad748
itskellyne <kellyne333@gmail.com>
2024-01-23 14:46:58 -0500

    in&out

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,8 @@
-// fetchData.js
+import { fetchData } from "./fetch_functions"
 
-const allDesserts = fetchData('')
+const allDesserts = fetchData('https://www.themealdb.com/api/json/v1/1/filter.php?c=dessert')
 allDesserts.forEach((dessert) => {
-    
+    document.querySelector()
 })
 
 
