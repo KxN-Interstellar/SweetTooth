79219f075dad7c174d87f6a12091612e898dfd01
itskellyne <kellyne333@gmail.com>
2024-03-13 16:09:51 -0400

    the fix was in the vite config file

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   // GitHub Pages expects an index.html in the root directory
   // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: '..' },
+  build: {
+    outDir: "..",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        page1: "./src/desserts.html",
+        // page2: 'me.html',
+        // page3: 'verses.html'
+      },
+    },
+  },
   // needed for github pages just put the repo name here
-  base: '/SweetTooth/', 
+  base: "/SweetTooth/",
 });
